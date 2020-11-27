d3e2b951a60d6f25e8a799ce4875eda9b67b3270
Shamsullo <shamsulloismatov@gmail.com>
2020-11-27 13:31:26 +0600

    deleted venv from git

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!B:\AUCA\Software Engineering\Event App Landing Page\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
